b526fe44deebf2df8ceb22ee36d2e82a5c47ef77
puppyer <38995847+puppyer@users.noreply.github.com>
2019-01-15 11:44:55 +0800

    modified
    modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 let app = document.getElementById("app");
 //create 创建，此时div还未存在于页面，但存在于内存
 let div = document.createElement("div");
@@ -11,14 +9,14 @@ div.innerHTMl = `
 `;
 //mount 挂载
 app.appendChild(div);
-div.querySelector("button")[0].onclick = () => {
+div.querySelectorAll("button")[0].onclick = () => {
   state += 1;
   //update 更新
   div.querySelector("p").innerText = state;
 };
-div.querySelector("button")[1].onclick = () => {
-  div.querySelector("button")[0].onclick = null;
-  div.querySelector("button")[1].onclick = null;
+div.querySelectorAll("button")[1].onclick = () => {
+  div.querySelectorAll("button")[0].onclick = null;
+  div.querySelectorAll("button")[1].onclick = null;
   div.remove();
   //destory
   div = null;
