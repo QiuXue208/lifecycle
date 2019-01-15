2cd0f8132956c31b511e051a74110568c9a5edc0
puppyer <38995847+puppyer@users.noreply.github.com>
2019-01-15 11:37:19 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+let app = document.getElementById("app");
+//create 创建，此时div还未存在于页面，但存在于内存
+let div = document.createElement("div");
+let state = 0;
+div.innerHTMl = `
+  <p>${state}</p>
+  <button>+1</button>
+  <button>移除div</button>
+`;
+//mount 挂载
+app.appendChild(div);
+div.querySelector("button")[0].onclick = function() {
+  state += 1;
+  //update 更新
+  div.querySelector("p").innerText = state;
+};
+div.querySelector("button")[1].onclick = function() {
+  div.querySelector("button")[0].onclick = null;
+  div.querySelector("button")[1].onclick = null;
+  div.remove();
+  //destory
+  div = null;
+};
